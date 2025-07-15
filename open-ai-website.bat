875b719b63fa0ededa02cae7b8d83d998f62dda2
FangXiangmin <fangxiangmin1@163.com>
2025-07-15 16:18:04 +0800

    Create open-ai-website.bat

---
+++ open-ai-website.bat
@@ -0,0 +1,8 @@
+@echo off
+start https://chat.openai.com/chat
+start https://www.doubao.com/chat/
+start https://chat.deepseek.com/
+start https://chat.qwen.ai/
+start https://kimi.moonshot.cn/
+start https://grok.com/
+
